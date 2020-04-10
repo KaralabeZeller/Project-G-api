b709b66d778a87a44eb49620572b444be32c14f0
Rudolf Rákos <63420196+rakosrudolf@users.noreply.github.com>
2020-04-11 00:20:15 +0200

    Implement session / state restore support after WebSocket reconnect on chat.addUser

--- src/main/java/com/nter/projectg/controller/MessageController.java
+++ src/main/java/com/nter/projectg/controller/MessageController.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 import com.nter.projectg.common.Lobby;
@@ -16,16 +17,26 @@ import com.nter.projectg.model.GMessage.MessageType;
 
 @Controller
 public class MessageController {
+	
 	private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
 
 	private Game game;
 	
+	private GMessage startMessage;
+	private SimpMessageHeaderAccessor startMessageHeaderAccessor;	
+	
 	@MessageMapping("/chat.sendMessage")
 	@SendTo("/topic/public")
-	public GMessage sendMessage(@Payload GMessage chatMessage) {
+	public GMessage sendMessage(@Payload GMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
 		
-		if(chatMessage.getType() == MessageType.START)
+		if(chatMessage.getType() == MessageType.START) {
+			startMessage = chatMessage;
+			startMessageHeaderAccessor = headerAccessor;
 			game = new SecretHitler();
+		}
 		
 		return chatMessage;
 	}
@@ -35,18 +46,25 @@ public class MessageController {
 	public GMessage addUser(@Payload GMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
 		// Add username in web socket session
 		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		
 		Lobby.addUser(chatMessage.getSender());
 		logger.info("Users: " + Lobby.size());
 
+		if (game != null /* && startMessage != null &&  startMessageHeaderAccessor != null */) {
+			GMessage reconnectMessage = startMessage;
+			SimpMessageHeaderAccessor reconnectMessageHeaderAccessor = SimpMessageHeaderAccessor.create();
+			reconnectMessageHeaderAccessor.copyHeaders(startMessageHeaderAccessor.toMap());
+			reconnectMessageHeaderAccessor.setSessionId(headerAccessor.getSessionId());
+			messagingTemplate.convertAndSendToUser(headerAccessor.getSessionId(), reconnectMessageHeaderAccessor.getDestination(), reconnectMessage, reconnectMessageHeaderAccessor.toMap());
+		}		
+		
 		String message = "";
 		for (String user : Lobby.getUsers()) {
 			message += user + ",";
 
 		}
-
 		chatMessage.setContent(message.substring(0, message.length() - 1));
 
-		logger.info("Users: " + chatMessage.getContent());
 		return chatMessage;
 	}
 
