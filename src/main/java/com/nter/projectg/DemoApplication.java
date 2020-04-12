073ebfdbc3b0d6968f07d0463626a7b62f623515
Rudolf Rákos <rakos.rudolf@hotmail.com>
2020-04-12 18:43:28 +0200

    Implement support for accessing logs via HTTP / REST
    Implement simple api-key authentication
    
    Routes:
      /log -> LogController
         GET -> LogController.get

--- src/main/java/com/nter/projectg/DemoApplication.java
+++ src/main/java/com/nter/projectg/DemoApplication.java
@@ -4,10 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
---
+++ src/main/java/com/nter/projectg/controller/LogControler.java
@@ -0,0 +1,52 @@
+package com.nter.projectg.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
+// TODO authenticate with API key using Spring Security
+@Controller
+@RestController
+@RequestMapping(path = "/log")
+public class LogControler {
+
+    private static final String API_KEY = "N7WxsJLRePC4ZLqy";
+    private static final String LOG_FILE = "/var/log/project-g-api.log";
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
+    @GetMapping(path = "", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> get(@RequestParam(name = "api-key", required = false) String apiKey) throws IOException {
+        if (authenticate(apiKey)) {
+            try {
+                logger.debug("Reading log file contents: {}", LOG_FILE);
+                List<String> lines = Files.readAllLines(Paths.get(LOG_FILE));
+                String contents = String.join("\n", lines);
+                return ResponseEntity.ok().body(contents);
+            } catch (Exception exception) {
+                logger.error("Failed to read log file contents: {}", LOG_FILE, exception);
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("500 Internal Server Error");
+            }
+        } else {
+            logger.debug("Rejecting request with invalid api-key to read log file contents");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("401 Not Authorized");
+        }
+
+    }
+
+    private boolean authenticate(String apiKey) {
+        return Objects.equals(API_KEY, apiKey);
+    }
+
+}
