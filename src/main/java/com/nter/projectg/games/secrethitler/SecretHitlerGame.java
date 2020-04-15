KaralabeZeller/Project-G-api

commit 3f7828d07c628538016d037031a54a33e31f316b

Move start and reconnect handling to Game from MessageController

src/main/java/com/nter/projectg/games/secrethitler/SecretHitlerGame.java
@@ -2,12 +2,14 @@ package com.nter.projectg.games.secrethitler;
 
 import com.nter.projectg.common.Lobby;
 import com.nter.projectg.games.common.Game;
-import com.nter.projectg.model.common.Message;
 import com.nter.projectg.model.secrethitler.SecretHitlerMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class SecretHitlerGame extends Game<SecretHitlerMessage, SecretHitlerPlayer> {
 
@@ -28,13 +30,12 @@ public class SecretHitlerGame extends Game<SecretHitlerMessage, SecretHitlerPlay
     private int alivePlayers;
 
     public SecretHitlerGame(Lobby lobby) {
-        super(lobby, "Secret Hitler", 5, 10);
+        super(lobby, "SecretHitler", 5, 10);
 
         logger.debug("Initializing Secret Hitler: {}", this);
 
         initializeAssets();
         initializeFactions();
-        electPresident();
         logger.info("Initialized Secret Hitler: {}", this);
     }
 
@@ -45,28 +46,21 @@ public class SecretHitlerGame extends Game<SecretHitlerMessage, SecretHitlerPlay
             Random rand = new Random();
             presidentID = rand.nextInt(players.size());
         } else {
+            int nextPresident = -1;
+            int candidate = lastNormalPresident + 1;
 
-            if (presidentID == -1) {
-                Random rand = new Random();
-                presidentID = rand.nextInt(playerCount);
-            } else {
+            while (nextPresident < 0) {
+                if (candidate >= playerCount)
+                    candidate = 0;
 
-                int nextPresident = -1;
-                int candidate = lastNormalPresident + 1;
-
-                while (nextPresident < 0) {
-                    if (candidate >= playerCount)
-                        candidate = 0;
-
-                    if (assets.playerMap.get(candidate) == 1)
-                        nextPresident = candidate;
-                    else
-                        candidate++;
-                }
-
-                presidentID = candidate;
-
+                if (assets.playerMap.get(candidate) == 1)
+                    nextPresident = candidate;
+                else
+                    candidate++;
             }
+
+            presidentID = candidate;
+
             lastNormalPresident = presidentID;
         }
 
@@ -163,20 +157,18 @@ public class SecretHitlerGame extends Game<SecretHitlerMessage, SecretHitlerPlay
             setChancellor(message.getContent());
         } else if (message.getGameMessageType() == SecretHitlerMessage.GameMessageType.VOTE) {
             votes.process(message.getSender(), message.getContent());
-            if(votes.voteFinished()) {
+            if (votes.voteFinished()) {
                 processVotes();
             }
-        }else {
+        } else {
             // TODO other messages
         }
     }
 
     private void processVotes() {
         logger.info("Voting has finished");
-        votes.getVotes().forEach((key,value) -> sendToAll(SecretHitlerMessage.GameMessageType.VOTED, key, value));
-
+        votes.getVotes().forEach((key, value) -> sendToAll(SecretHitlerMessage.GameMessageType.VOTED, key, value));
     }
-
 
     private void sendToAll(SecretHitlerMessage.GameMessageType type, String user, String content) {
         SecretHitlerMessage message = new SecretHitlerMessage();
@@ -187,14 +179,14 @@ public class SecretHitlerGame extends Game<SecretHitlerMessage, SecretHitlerPlay
     }
 
     private void setChancellor(String player) {
-        if(gameState != Constants.SHState.NOMINATION) {
+        if (gameState != Constants.SHState.NOMINATION) {
             logger.warn("Message received in a false state: {}", gameState.name());
             return;
         }
 
         logger.info("Nominated chancellor: {}", player);
-        for(int i = 0; i < players.size(); i++) {
-            if(players.get(i).getName().equals(player)) {
+        for (int i = 0; i < players.size(); i++) {
+            if (players.get(i).getName().equals(player)) {
                 chancellorID = i;
                 break;
             }
@@ -209,14 +201,52 @@ public class SecretHitlerGame extends Game<SecretHitlerMessage, SecretHitlerPlay
 
     private void voteGovernment() {
         logger.info("Voting for government");
-        for(SecretHitlerPlayer player: players) {
+        for (SecretHitlerPlayer player : players) {
             player.sendCommand(SecretHitlerMessage.GameMessageType.VOTE, "Ja!,Nein!");
         }
     }
 
     @Override
-    public void run() {
-        // TODO
+    public void start(String user) {
+        super.start(user);
+
+        SecretHitlerPlayer player = findPlayer(user);
+
+        // Send faction message to session
+        SecretHitlerMessage factionMessage = new SecretHitlerMessage();
+        factionMessage.setSender(getName());
+        factionMessage.setGameMessageType(SecretHitlerMessage.GameMessageType.FACTION);
+        factionMessage.setContent(player.getFaction().name());
+        sendToPlayer(player.getName(), factionMessage);
+
+        electPresident();
+    }
+
+    // TODO implement restore state / messages on reconnect
+    @Override
+    public void reconnect(String user) {
+        super.reconnect(user);
+
+        SecretHitlerPlayer player = findPlayer(user);
+
+        // Send faction message to session
+        SecretHitlerMessage factionMessage = new SecretHitlerMessage();
+        factionMessage.setSender(getName());
+        factionMessage.setGameMessageType(SecretHitlerMessage.GameMessageType.FACTION);
+        factionMessage.setContent(player.getFaction().name());
+        sendToPlayer(player.getName(), factionMessage);
+
+        // Send president message to session
+        SecretHitlerMessage presidentMessage = new SecretHitlerMessage();
+        presidentMessage.setSender(getName());
+        presidentMessage.setGameMessageType(SecretHitlerMessage.GameMessageType.PRESIDENT);
+        presidentMessage.setContent(players.get(presidentID).getName());
+        sendToPlayer(player.getName(), presidentMessage);
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
     }
 
     @Override

src/main/java/com/nter/projectg/controller/MessageController.java
@@ -26,9 +26,6 @@ public class MessageController {
     private Lobby lobby;
 
     private Game<?, ?> game;
-    // TODO move to Game from MessageController
-    private Message startMessage;
-    private Message gameMessage;
 
     // TODO use /app/game application destination
     @MessageMapping("/chat.sendMessage")
@@ -46,19 +43,12 @@ public class MessageController {
 
         // Handle message and update state
         if (message.getType() == MessageType.START) {
-            start();
-            startMessage = message;
-
-            // Broadcast message to all sessions - Not needed for every message type
-            lobby.sendToAll(message);
+            start(user);
         } else if (message.getType() == MessageType.GAME) {
             process(message);
-            gameMessage = message;
         } else {
             // TODO other messages
         }
-
-
     }
 
     // TODO use /app/lobby application destination
@@ -79,15 +69,12 @@ public class MessageController {
         reconnect(user, session);
     }
 
-    private CompletableFuture<Void> start() {
+    private CompletableFuture<Void> start(String user) {
         // Fake asynchronous computation
         return CompletableFuture.runAsync(() -> {
             logger.debug("Starting game: Secret Hitler {}", lobby);
-
             game = new SecretHitlerGame(lobby);
-            // TODO implement
-            // game.start(user);
-
+            game.start(user);
             logger.info("Started game: Secret Hitler {}", game);
 
             // Fake timeout to reset Game
@@ -98,11 +85,8 @@ public class MessageController {
             }
 
             logger.debug("Stopping game: Secret Hitler {}", game);
-
-            // TODO implement
-            // game.stop(user);
+            game.stop();
             game = null;
-
             logger.debug("Stopped game: Secret Hitler {}", lobby);
         });
     }
@@ -126,17 +110,7 @@ public class MessageController {
         return CompletableFuture.runAsync(() -> {
             if (game != null) {
                 logger.debug("Reconnecting user in session: {} {}", user, session);
-
-                // TODO implement restore state / messages on reconnect
-                if (startMessage != null) {
-                    lobby.sendToUser(user, startMessage);
-                    if (gameMessage != null) {
-                        lobby.sendToUser(user, gameMessage);
-                    }
-                }
-
                 game.reconnect(user);
-
                 logger.info("Reconnected user in session: {} {}", user, session);
             }
         });

src/main/java/com/nter/projectg/games/common/Game.java
@@ -8,9 +8,10 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 
-public abstract class Game<GameMessage extends Message, GamePlayer extends Player<GameMessage>> implements Runnable {
+public abstract class Game<GameMessage extends Message, GamePlayer extends Player<GameMessage>> {
 
     private static final Logger logger = LoggerFactory.getLogger(Game.class);
 
@@ -37,6 +38,7 @@ public abstract class Game<GameMessage extends Message, GamePlayer extends Playe
     public String getName() {
         return name;
     }
+
     public int getPlayerCount() {
         return players.size();
     }
@@ -45,12 +47,23 @@ public abstract class Game<GameMessage extends Message, GamePlayer extends Playe
         return Collections.unmodifiableList(players);
     }
 
+    protected GamePlayer findPlayer(String name) {
+        for (GamePlayer player : players) {
+            if (Objects.equals(player.getName(), name)) {
+                return player;
+            }
+        }
+
+        throw new IllegalArgumentException(String.format("Failed to find player: Invalid name: %s", name));
+    }
+
     private void initializePlayers() {
         logger.debug("Initializing players: {}", players);
 
         int userCount = lobby.getUsers().size();
         if (userCount < minPlayers || userCount >= maxPlayers) {
             logger.warn("Player count is not in the interval: {} < {} < {}", minPlayers, userCount, maxPlayers);
+            // TODO ignore invalid state
         }
 
         for (String user : lobby.getUsers()) {
@@ -65,7 +78,6 @@ public abstract class Game<GameMessage extends Message, GamePlayer extends Playe
     protected void sendToAll(GameMessage message) {
         lobby.sendToAll(message);
     }
-
 
 
     protected void sendToPlayer(String name, GameMessage message) {
@@ -92,13 +104,31 @@ public abstract class Game<GameMessage extends Message, GamePlayer extends Playe
 
     protected abstract void processMessage(GameMessage message);
 
-    public void reconnect(String user) {
-        // TODO implement
+    public void start(String user) {
+        GamePlayer player = findPlayer(user);
+
+        // Broadcast start message to all sessions
+        Message message = new Message();
+        message.setType(Message.MessageType.START);
+        message.setSender(player.getName());
+        message.setContent(name);
+        lobby.sendToAll(message);
     }
 
-    @Override
-    public void run() {
-        // TODO
+    // TODO implement restore state / messages on reconnect
+    public void reconnect(String user) {
+        GamePlayer player = findPlayer(user);
+
+        // Send start message to all sessions
+        Message message = new Message();
+        message.setType(Message.MessageType.START);
+        message.setSender(player.getName());
+        message.setContent(name);
+        lobby.sendToUser(player.getName(), message);
+    }
+
+    public void stop() {
+        // TODO implement
     }
 
     // TODO maybe remove lobby

src/main/java/com/nter/projectg/games/secrethitler/SecretHitlerPlayer.java
@@ -20,12 +20,7 @@ public class SecretHitlerPlayer extends Player<SecretHitlerMessage> {
     public void setFaction(Constants.Faction faction) {
         this.faction = faction;
 
-        // Send message to session
-        SecretHitlerMessage message = new SecretHitlerMessage();
-        message.setSender(getName());
-        message.setGameMessageType(SecretHitlerMessage.GameMessageType.FACTION);
-        message.setContent(faction.name());
-        send(message);
+
     }
 
     public void sendCommand(SecretHitlerMessage.GameMessageType type, String content) {

commit db93a2ca12c54937a58ab8f40337e1a2842cc7ff

Fix sending faction message to players on game start caused by 3f7828d0

src/main/java/com/nter/projectg/games/secrethitler/SecretHitlerGame.java
@@ -214,14 +214,14 @@ public class SecretHitlerGame extends Game<SecretHitlerMessage, SecretHitlerPlay
     public void start(String user) {
         super.start(user);
 
-        SecretHitlerPlayer player = findPlayer(user);
-
         // Send faction message to session
-        SecretHitlerMessage factionMessage = new SecretHitlerMessage();
-        factionMessage.setSender(getName());
-        factionMessage.setGameType(GameMessageType.FACTION);
-        factionMessage.setContent(player.getFaction().name());
-        sendToPlayer(player.getName(), factionMessage);
+        for (SecretHitlerPlayer player : players) {
+            SecretHitlerMessage factionMessage = new SecretHitlerMessage();
+            factionMessage.setSender(getName());
+            factionMessage.setGameType(GameMessageType.FACTION);
+            factionMessage.setContent(player.getFaction().name());
+            sendToPlayer(player.getName(), factionMessage);
+        }
 
         electPresident();
     }