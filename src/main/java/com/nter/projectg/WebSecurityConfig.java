8769580a1587dbad9a01ba42606d54cf00b56e46
KaralabeZeller <g.fertetics@gmail.com>
2020-04-24 12:09:26 +0200

    Force https

--- src/main/java/com/nter/projectg/WebSecurityConfig.java
+++ src/main/java/com/nter/projectg/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();
-        http.requiresChannel().antMatchers("/login*").requiresSecure();
+
         http.authorizeRequests()
                 .antMatchers("/registration", "/").permitAll()
                 .anyRequest().authenticated()
