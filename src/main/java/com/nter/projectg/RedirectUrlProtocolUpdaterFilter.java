5d80d0fb74308e0cdb507304ae2110b25b38055b
KaralabeZeller <g.fertetics@gmail.com>
2020-04-24 18:55:02 +0200

    Location header update to https

---
+++ src/main/java/com/nter/projectg/RedirectUrlProtocolUpdaterFilter.java
@@ -0,0 +1,41 @@
+package com.nter.projectg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE)
+class RedirectUrlProtocolUpdaterFilter extends OncePerRequestFilter {
+    private static final Logger logger = LoggerFactory.getLogger(RedirectUrlProtocolUpdaterFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String locationHeader = response.getHeader("Location");
+        logger.info("############ inside interceptor");
+
+        for(String name: response.getHeaderNames()) {
+            logger.info(name + " : " + response.getHeader(name));
+        }
+
+        if(locationHeader != null && locationHeader.startsWith("http://")) {
+            logger.info("###################### setting location header");
+
+            locationHeader = locationHeader.replaceAll("http://", "https://");
+            response.setHeader("Location", locationHeader);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
