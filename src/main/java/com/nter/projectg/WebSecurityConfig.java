cc36c8749978e599eda0b4540a6f38872f1f0b49
KaralabeZeller <g.fertetics@gmail.com>
2020-04-24 22:19:08 +0200

    Location header update to https

--- src/main/java/com/nter/projectg/WebSecurityConfig.java
+++ src/main/java/com/nter/projectg/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -71,14 +70,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .and().httpBasic();
-
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);;
+                .logoutSuccessUrl("/");
 
 
     }
-
 
 
 
