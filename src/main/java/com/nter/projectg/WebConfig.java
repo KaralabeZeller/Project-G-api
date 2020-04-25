73c3ca28a2c3dd3065625c979d5f47bcfbda5628
KaralabeZeller <g.fertetics@gmail.com>
2020-04-25 13:40:50 +0200

    Removed unnecessary logs

--- src/main/java/com/nter/projectg/WebConfig.java
+++ src/main/java/com/nter/projectg/WebConfig.java
@@ -10,10 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("login");
 
-    }
 
     @Bean
     public CommonsRequestLoggingFilter requestLoggingFilter() {
