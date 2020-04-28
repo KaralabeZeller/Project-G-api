ed23e88c8739d47ae73ab6d09a8243239578e36e
KaralabeZeller <g.fertetics@gmail.com>
2020-04-28 21:26:00 +0200

    Fix for login error

---
+++ src/main/java/com/nter/projectg/handler/MyAuthFailureHandler.java
@@ -0,0 +1,44 @@
+package com.nter.projectg.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyAuthFailureHandler implements AuthenticationFailureHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final Logger logger = LoggerFactory.getLogger(MyAuthFailureHandler.class);
+
+    @Override
+    public void onAuthenticationFailure(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException exception)
+            throws IOException, ServletException {
+        String environment = request.getHeader("host").contains("localhost") ? "LOCAL" : "REMOTE";
+        logger.info("onAuthenticationFailure() environment: {}", environment);
+
+        String redirectUrl;
+        if (environment.equals("LOCAL")) {
+            redirectUrl = request.getScheme() + "://" + request.getServerName() + (request.getServerPort() != 80 ? ":" + request.getServerPort() : "") + "/login?error";
+        } else {
+            redirectUrl = "https://" + request.getServerName() + ":443/login?error";
+        }
+        logger.info("onAuthenticationFailure() redirectUrl: {}", redirectUrl);
+
+        response.sendRedirect(redirectUrl);
+    }
+
+
+}
