b412071af5414c79dc0ed39c3ef6e1aef8f480fd
KaralabeZeller <g.fertetics@hotmail.com>
2020-05-03 01:14:37 +0200

    Further improve WebRTC MVP MNY TLA

--- src/main/resources/static/js/call.js
+++ src/main/resources/static/js/call.js
@@ -12,7 +12,43 @@ function initCall(_send) {
     callButton.onclick = call;
     hangupButton.onclick = hangup;
     const send = _send;
-    const servers = null;
+    const servers = {iceServers: [
+    { urls: 'stun:stun.l.google.com:19302'},
+    {urls:'stun:stun01.sipphone.com'},
+    {urls:'stun:stun.ekiga.net'},
+    {urls:'stun:stun.fwdnet.net'},
+    {urls:'stun:stun.ideasip.com'},
+    {urls:'stun:stun.iptel.org'},
+    {urls:'stun:stun.rixtelecom.se'},
+    {urls:'stun:stun.schlund.de'},
+    {urls:'stun:stun.l.google.com:19302'},
+    {urls:'stun:stun1.l.google.com:19302'},
+    {urls:'stun:stun2.l.google.com:19302'},
+    {urls:'stun:stun3.l.google.com:19302'},
+    {urls:'stun:stun4.l.google.com:19302'},
+    {urls:'stun:stunserver.org'},
+    {urls:'stun:stun.softjoys.com'},
+    {urls:'stun:stun.voiparound.com'},
+    {urls:'stun:stun.voipbuster.com'},
+    {urls:'stun:stun.voipstunt.com'},
+    {urls:'stun:stun.voxgratia.org'},
+    {urls:'stun:stun.xten.com'},
+    {
+    	urls: 'turn:numb.viagenie.ca',
+    	credential: 'muazkh',
+    	username: 'webrtc@live.com'
+    },
+    {
+    	urls: 'turn:192.158.29.39:3478?transport=udp',
+    	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    	username: '28224511:1379330808'
+    },
+    {
+    	urls: 'turn:192.158.29.39:3478?transport=tcp',
+    	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    	username: '28224511:1379330808'
+    }
+    ]};
 
     var lobbyName = document.getElementById('lobbyName').value;
 
@@ -79,36 +115,44 @@ function initCall(_send) {
       console.log(`Failed to create session description: ${error.toString()}`);
     }
 
+
+    //CALLER
     function sendOffer(desc) {
-      console.log(`Offer from pc1Local\n${desc.sdp}`);
+      console.log(`sendOffer\n${desc.sdp}`);
       pc1Local.setLocalDescription(desc, ()=>{}, onCreateSessionDescriptionError);
       send(desc, 'OFFER');
 
     }
 
+    function gotAnswer(desc) {
+      console.log(`gotAnswer()\n${desc.sdp}`);
+      pc1Local.setRemoteDescription(desc, ()=>{}, onCreateSessionDescriptionError);
+    }
+
+    //RECEIVER
     function gotOffer(desc) {
-      console.log(`offer from pc1Remote\n${desc.sdp}`);
+      console.log(`gotOffer()\n${desc.sdp}`);
       pc1Remote.setRemoteDescription(desc, createAnswer, onCreateSessionDescriptionError);
 
-
     }
-    function createAnswer() {
-        console.log(`Create answer to pc1Remote`);
-        pc1Remote.createAnswer(sendAnswer, onCreateSessionDescriptionError);
+    function setLocal(desc) {
+        console.log(`setLocal()`);
+        pc1Remote.setLocalDescription(desc, ()=>{}, onCreateSessionDescriptionError);
+    }
+
+    function createAnswer(desc) {
+        console.log(`createAnswer()`);
+        pc1Remote.createAnswer(sendAnswer, onCreateSessionDescriptionError).then( () => setLocal(desc));
 
     }
 
     function sendAnswer(desc) {
-      console.log(`Answer from pc1Remote\n${desc.sdp}`);
+      console.log(`sendAnswer()\n${desc.sdp}`);
+
       send(desc, 'ANSWER');
-      pc1Remote.setLocalDescription(desc, ()=>{}, onCreateSessionDescriptionError);
 
     }
 
-    function gotAnswer(desc) {
-      console.log(`Answer from pc2Remote\n${desc.sdp}`);
-      pc1Local.setRemoteDescription(desc, ()=>{}, onCreateSessionDescriptionError);
-    }
 
 
     function hangup() {
