24f2fe611399aa8851d2961bcc5161dd2fe2649c
KaralabeZeller <g.fertetics@hotmail.com>
2020-05-04 17:56:27 +0200

    making the ui more consistent

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,8 @@
+document.getElementById("navMenu").innerHTML =
+ '<div class="dropdown">' +
+   '<button class="dropbtn">Menu</button>' +
+   '<div class="dropdown-content">' +
+     '<a href="/logout">Logout</a>' +
+     '<a href="/lobbies">Lobbies</a>' +
+   '</div>' +
+ '</div>';
